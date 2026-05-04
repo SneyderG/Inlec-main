96ae3ff8e31e09e50240cd2413ab0c64c02d96ec
lu16253-nh <sneyder.gaviria180107@gmail.com>
2026-05-04 10:37:25 -0500

    ggg

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import {App} from './App.jsx'
+import {AppMain} from './AppMain.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*<App />*/}
     <AppMain />
   </StrictMode>,
 )
